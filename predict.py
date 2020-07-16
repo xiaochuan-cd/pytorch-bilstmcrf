6d4ba8e39828905115a7a88385250217e868908c
zhangyang <zhangyang@chinamcloud.com>
2020-07-16 16:03:53 +0800

    fix

--- predict.py
+++ predict.py
@@ -77,8 +77,8 @@ group.add("--bidirectional", action="store_true", default=False,
 def main(args):
     logging.basicConfig(level=logging.INFO)
 
-    label_vocab = pickle.load(open(os.path.join(args.save_dir, "vocab-input.pkl"), 'rb'))
-    input_vocabs = [pickle.load(open(os.path.join(args.save_dir, "vocab-label.pkl"), 'rb'))]
+    label_vocab = pickle.load(open(os.path.join(args.save_dir, "vocab-label.pkl"), 'rb'))
+    input_vocabs = [pickle.load(open(os.path.join(args.save_dir, "vocab-input.pkl"), 'rb'))]
 
     logging.info("Initializing model...")
     crf = M.CRF(len(label_vocab))
